ffac38953c58eb5a842cab3695830943079cc8c7
Jourdan Templeton <hello@jourdant.me>
2015-11-03 02:04:09 +1300

    rebuilt project structure to match proper node dependency tree, updated loader code to import from that structure

--- adash.js
+++ adash.js
@@ -1,60 +1,78 @@
 var fs = require('fs');
 var path_module = require('path');
 
+function log(e,t){var g=new Date;if(g="["+g.getFullYear()+"/"+(g.getMonth()<10?"0"+g.getMonth():g.getMonth())+"/"+(g.getDate()<10?"0"+g.getDate():g.getDate())+" "+(g.getHours()<10?"0"+g.getHours():g.getHours())+":"+(g.getMinutes()<10?"0"+g.getMinutes():g.getMinutes())+":"+(g.getSeconds()<10?"0"+g.getSeconds():g.getSeconds())+"]",e.length<15)for(i=e.length;i<10;i++)e+=" ";console.log(g+" ["+e.toUpperCase()+"] ~ "+t)}
 
-//application state
-var handlers = {};
-var monitors = {};
-
-
-function log(tag, message) {
-    var date = new Date();
-    date = '[' + date.getFullYear() + '/' + (date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()) + '/' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ' + (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':' + (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()) + ']';
-
-    //pad the tag
-    if (tag.length < 11) {for (i = tag.length;i<10;i++) {tag += ' ';}}
-
-    console.log(date + ' [' + tag.toUpperCase() + '] ~ ' + message);
-}
-
-function loadModules(base) {
+function loadModules(base, prefix) {
 	var ret = {};
 
 	//check base dir
 	if (fs.lstatSync(base).isDirectory() == true) {
 		var dirs = fs.readdirSync(base);
 		for (subdir in dirs) {
-			subdir = dirs[subdir];
+			if (dirs[subdir].substring(0,prefix.length).indexOf(prefix) !== -1) {
+				//load modules from sub dirs with prefix
+				var path = path_module.join(base, dirs[subdir]);
+				if (fs.lstatSync(path).isDirectory() == true) {
+					var files = fs.readdirSync(path);
 
-			//load modules from sub dirs
-			var path = path_module.join(base, subdir);
-			if (fs.lstatSync(path).isDirectory() == true) {
-				var files = fs.readdirSync(path);
+					for (file in files) {
+						file = files[file];
 
-				for (file in files) {
-					file = files[file];
-
-					//only loading .js files
-					if (file.indexOf('.js', file.length - 3) !== -1) {
-						var module = require(path_module.join(path, file))(log);
-						ret[module.name] = module;
-					}
-				}
-			}
-		}
-	}
+						//only loading .js files
+						if (file.indexOf('.js', file.length - 3) !== -1) {
+							var full = path_module.join(path, file);
+							var module = require(full)(full, log);
+							ret[module.mac] = module;
+		}}}}}}
 	return ret;
 }
 
-var monitors_dir = path_module.join(__dirname, 'monitors');
-var monitors = loadModules(monitors_dir);
-
-
 function handler(source, mac) {
-	log("HANDLER", "[" + source.name + "] " + mac);
+	var beacon = beacons[mac];
+	if (beacon != null ) {
+		var date = new Date();
+		if (beacon.last_triggered == null || Math.abs(date - beacon.last_triggered) > 15000) {
+			beacon.last_triggered = date;
+			log('event', "Beacon '" + beacon.name + "' pushed.");
+			beacon.execute();
+			return;
+		}
+	}
 }
 
-for (mon in triggers) {
-	mon = monitors[mon];
-	mon.start(handler);
+
+
+
+//=====
+//application logic
+//=====
+
+//state
+var modules_dir = path_module.join(__dirname, 'node_modules');
+var handlers = {};
+var monitors = {};
+
+//setup
+console.log("[adash.js]");
+log('setup', 'Loading triggers...');
+var triggers = loadModules(modules_dir, 'trigger-');
+for (trigger in triggers) { log('setup', '    (*) ' + triggers[trigger].name); }
+log('setup', Object.keys(triggers).length + ' trigger' + (Object.keys(triggers).length > 1 ? 's' : '') + ' loaded.');
+
+
+log('setup', 'Loading beacons...');
+var beacons = loadModules(modules_dir, 'beacon-');
+for (beacon in beacons) { log('setup', '    (*) ' + beacons[beacon].mac + ' - ' + beacons[beacon].name); }
+log('setup', Object.keys(beacons).length + ' beacon' + (Object.keys(beacons).length > 1 || Object.keys(beacons).length < 1 ? 's' : '') + ' loaded.');
+
+
+
+//start the triggers!
+for (trigger in triggers) {
+	trigger = triggers[trigger];
+	trigger.start(handler);
 }
+
+
+log('setup', 'Ready!\r\n');
