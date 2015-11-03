b69d3723105601a59d6c7759b778333325d590c8
Jourdan Templeton <hello@jourdant.me>
2015-11-03 13:47:08 +1300

    Added provisional web server trigger support. Web server can load list of beacons and can trigger a 'pushed' event by sending http request to /{macaddress} of webserver

--- adash.js
+++ adash.js
@@ -3,7 +3,7 @@ var path_module = require('path');
 
 function log(e,t){var g=new Date;if(g="["+g.getFullYear()+"/"+(g.getMonth()<10?"0"+g.getMonth():g.getMonth())+"/"+(g.getDate()<10?"0"+g.getDate():g.getDate())+" "+(g.getHours()<10?"0"+g.getHours():g.getHours())+":"+(g.getMinutes()<10?"0"+g.getMinutes():g.getMinutes())+":"+(g.getSeconds()<10?"0"+g.getSeconds():g.getSeconds())+"]",e.length<15)for(i=e.length;i<10;i++)e+=" ";console.log(g+" ["+e.toUpperCase()+"] ~ "+t)}
 
-function loadModules(base, prefix) {
+function loadModules(base, prefix, index) {
 	var ret = {};
 
 	//check base dir
@@ -23,20 +23,21 @@ function loadModules(base, prefix) {
 						if (file.indexOf('.js', file.length - 3) !== -1) {
 							var full = path_module.join(path, file);
 							var module = require(full)(full, log);
-							ret[module.mac] = module;
-		}}}}}}
+							if (module != null) { 
+								ret[module[index].toLowerCase()] = module;
+		}}}}}}}
 	return ret;
 }
 
 function handler(source, mac) {
-	var beacon = beacons[mac];
+	var beacon = beacons[mac.toLowerCase()];
+
 	if (beacon != null ) {
 		var date = new Date();
 		if (beacon.last_triggered == null || Math.abs(date - beacon.last_triggered) > 15000) {
 			beacon.last_triggered = date;
 			log('event', "Beacon '" + beacon.name + "' pushed.");
 			beacon.execute();
-			return;
 		}
 	}
 }
@@ -44,9 +45,10 @@ function handler(source, mac) {
 
 
 
-//=====
-//application logic
-//=====
+
+//====================
+// application logic
+//====================
 
 //state
 var modules_dir = path_module.join(__dirname, 'node_modules');
@@ -56,13 +58,13 @@ var monitors = {};
 //setup
 console.log("[adash.js]");
 log('setup', 'Loading triggers...');
-var triggers = loadModules(modules_dir, 'trigger-');
+var triggers = loadModules(modules_dir, 'trigger-', 'name');
 for (trigger in triggers) { log('setup', '    (*) ' + triggers[trigger].name); }
 log('setup', Object.keys(triggers).length + ' trigger' + (Object.keys(triggers).length > 1 ? 's' : '') + ' loaded.');
 
 
 log('setup', 'Loading beacons...');
-var beacons = loadModules(modules_dir, 'beacon-');
+var beacons = loadModules(modules_dir, 'beacon-', 'mac');
 for (beacon in beacons) { log('setup', '    (*) ' + beacons[beacon].mac + ' - ' + beacons[beacon].name); }
 log('setup', Object.keys(beacons).length + ' beacon' + (Object.keys(beacons).length > 1 || Object.keys(beacons).length < 1 ? 's' : '') + ' loaded.');
 
@@ -76,3 +78,4 @@ for (trigger in triggers) {
 
 
 log('setup', 'Ready!\r\n');
+
